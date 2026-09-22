b29c1d34bce49dc41f7764eea7b1d306e63d2ae4
mhussainilyas <mhussainilyas7@gmail.com>
2026-09-22 11:32:52 +0500

    update

--- src/04_union_type.ts
+++ src/04_union_type.ts
@@ -10,3 +10,20 @@ data = "Hussain"; // ✔ allowed
 // data = true; // ❌ Error
 
 console.log(data);
+
+// ======================
+//     With Functions
+// ======================
+
+function addTwoItems(a: number | string, b: number | string): number | string {
+  if (typeof a === "string" || typeof b === "string") {
+    return a.toString() + b.toString();
+  } else {
+    return a + b;
+  }
+}
+
+console.log(addTwoItems(10, "595"));
+console.log(addTwoItems(10, 595));
+console.log(addTwoItems("10", "595"));
+console.log(addTwoItems("10", 595));
